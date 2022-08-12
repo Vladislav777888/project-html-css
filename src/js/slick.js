2759120ce366b47d25890fedca63a5b822679bee
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2022-08-12 16:51:04 +0300

    Розмітка+ базові стилі (1)

---
+++ src/js/slick.js
@@ -0,0 +1,10 @@
+$('.one-time').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: false,
+  autoplaySpeed: 4000,
+  arrows: false
+});
